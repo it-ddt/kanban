4e79e719f5d257cb9c2e4457c419a1a86ae8f37c
oli <72987619+olivieshek@users.noreply.github.com>
2023-06-05 15:27:43 +0300

    форма с русскими лейблами

--- forms.py
+++ forms.py
@@ -2,10 +2,40 @@ from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 
+
 class SignUpForm(UserCreationForm):
-    first_name = forms.CharField(max_length=30, required=False, help_text='Optional')
-    last_name = forms.CharField(max_length=30, required=False, help_text='Optional')
-    email = forms.EmailField(max_length=254, help_text='Enter a valid email address')
+    username = forms.CharField(
+        max_length=30,
+        required=True,
+        help_text='Обязательное поле. 150 символов и меньше. Буквы, цифры, специальные символы (@/./+/-/_).',
+        label="Имя пользователя",
+    )
+    first_name = forms.CharField(
+        max_length=30,
+        required=False,
+        help_text='Необязательное поле.',
+        label="Имя",
+    )
+    last_name = forms.CharField(
+        max_length=30,
+        required=False,
+        help_text='Необязательное поле.',
+        label="Фамилия",
+    )
+    email = forms.EmailField(
+        max_length=254,
+        help_text='Введите существующий адрес электронной почты.',
+        label="Адрес электронной почты"
+    )
+    password1 = forms.CharField(
+        widget=forms.PasswordInput,
+        help_text="<li>Ваш пароль должен содержать как минимум 5 символов.</li>",
+        label="Пароль",
+    )
+    password2 = forms.CharField(
+        widget=forms.PasswordInput,
+        label="Подтверждение пароля",
+    )
 
     class Meta:
         model = User
