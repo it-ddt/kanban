aa20b6706199c540b7a58e95d7df489332789558
it-ddt <45958272+it-ddt@users.noreply.github.com>
2023-06-10 00:37:37 +0300

    Полировка темплейтов + стили

--- forms.py
+++ forms.py
@@ -7,42 +7,25 @@ class SignUpForm(UserCreationForm):
     username = forms.CharField(
         max_length=30,
         required=True,
-        help_text='Обязательное поле. 150 символов и меньше. Буквы, цифры, специальные символы (@/./+/-/_).',
-        label="Имя пользователя",
-    )
-    first_name = forms.CharField(
-        max_length=30,
-        required=False,
-        help_text='Необязательное поле.',
-        label="Имя",
-    )
-    last_name = forms.CharField(
-        max_length=30,
-        required=False,
-        help_text='Необязательное поле.',
-        label="Фамилия",
+        label="имя",
     )
     email = forms.EmailField(
         max_length=254,
-        help_text='Введите существующий адрес электронной почты.',
-        label="Адрес электронной почты"
+        label="электронная почта"
     )
     password1 = forms.CharField(
         widget=forms.PasswordInput,
-        help_text="<li>Ваш пароль должен содержать как минимум 5 символов.</li>",
-        label="Пароль",
+        label="пароль",
     )
     password2 = forms.CharField(
         widget=forms.PasswordInput,
-        label="Подтверждение пароля",
+        label="повторите пароль",
     )
 
     class Meta:
         model = User
         fields = [
-            'username', 
-            'first_name', 
-            'last_name', 
+            'username',
             'email', 
             'password1', 
             'password2', 
